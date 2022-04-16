6556b0b93f52fcd30c20ae3f86c3c2755745bf56
lucyfurt <lucianafurtado10@gmail.com>
2022-04-16 02:13:50 -0300

    product entity, data script

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into tb_category(name, created_At) values ('books', NOW());
-insert into tb_category(name, created_At) values ('eletronics', NOW());
-insert into tb_category(name, created_At) values ('computers', NOW());
